309a0c1bfa16d3af854521992c7a416752a9e5f5
Aryan <Aryan.r.t@hotmail.com>
2020-03-26 18:20:16 -0400

    Bug: Edit product/shop should be disabled in Entity Details pages if enttity is not owned by logged in user

--- src/main/webapp/app/entities/product/product-detail.component.ts
+++ src/main/webapp/app/entities/product/product-detail.component.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from 'app/shared/model/product.model';
+import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'jhi-product-detail',
   templateUrl: './product-detail.component.html'
 })
 export class ProductDetailComponent implements OnInit {
-  product: IProduct | null = null;
+  product: IProduct | undefined;
+  currentLogin: string | undefined;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.accountService.identity().subscribe(account => {
+      this.currentLogin = account?.login;
+      //account?.login
+    });
     this.activatedRoute.data.subscribe(({ product }) => (this.product = product));
   }
 
--- src/main/webapp/app/entities/shop/shop-detail.component.ts
+++ src/main/webapp/app/entities/shop/shop-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { IShop } from 'app/shared/model/shop.model';
+import { AccountService } from 'app/core/auth/account.service';
 
 @Component({
   selector: 'jhi-shop-detail',
@@ -9,10 +10,15 @@ import { IShop } from 'app/shared/model/shop.model';
 })
 export class ShopDetailComponent implements OnInit {
   shop: IShop | null = null;
+  currentLogin: string | undefined;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(protected activatedRoute: ActivatedRoute, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.accountService.identity().subscribe(account => {
+      this.currentLogin = account?.login;
+      //account?.login
+    });
     this.activatedRoute.data.subscribe(({ shop }) => (this.shop = shop));
   }
 
